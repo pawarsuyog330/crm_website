e1bbf070a03c6c6e6362ec968be2c8d24e057726
Suyog Pawar <pawarsuyog330@gmail.com>
2023-11-07 20:21:46 +0530

    file added

--- website/views.py
+++ website/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', locals())
+    # check to see if user is logged in
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # authenticate user details
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'User logged in successfully!')
+            return redirect('home')
+        else:
+            messages.warning(request, 'Invalid username or password. Please try again.')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', locals())
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out successfully!')
+    return redirect('home')
+
+
+def register(request):
+    return render(request, 'register.html', locals())
